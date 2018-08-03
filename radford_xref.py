af61281b173f5f2212bedada30633c99e1bc696b
Tad Peterson <tadworkswith@gmail.com>
2018-08-02 19:32:03 -0500

    Initial commit. Added kill method dict.

---
+++ radford_xref.py
@@ -0,0 +1,22 @@
+kill_method_dict = {
+    '0'  : 'Unknown',
+    '1'  : 'Blunt force',
+    '2'  : 'Gunshot',
+    '3'  : 'Poinson-Non Medical',
+    '4'  : 'Sharp force-stabbing',
+    '5'  : 'Strangulation',
+    '6'  : 'Poison-Medical',
+    '7'  : 'Incendiary device',
+    '8'  : 'Suffocation',
+    '9'  : 'Gassed',
+    '10' : 'Drowning',
+    '11' : 'Fire',
+    '12' : 'Starvation',
+    '13' : 'Shaken Baby Syndrome',
+    '14' : 'Sharp force-Chop',
+    '15' : 'Hanging',
+    '16' : 'Ordered hit',
+    '17' : 'Staged accident',
+    '18' : 'Vehicle hit',
+    '19' : 'Thrown from cliff'
+}
